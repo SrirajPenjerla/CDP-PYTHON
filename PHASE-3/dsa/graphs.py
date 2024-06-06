013b49b86a2b692b3c33b653fb28c6b32abffc16
SrirajPenjerla <srirajpenejerla2003@gmail.com>
2024-06-06 14:44:23 +0530

    DFS

--- PHASE-3/dsa/graphs.py
+++ PHASE-3/dsa/graphs.py
@@ -9,6 +9,28 @@ def BFS(start,graph):
                 visited.append(i)
     return visited
 
+def DFS(start,graph):
+    ans=[]
+    visited=[start]
+    stack=[start]
+    while stack:
+        ele=stack.pop()
+        ans.append(ele)
+        for i in graph[ele]:
+            if i not in visited:
+                stack.append(i)
+                visited.append(i)
+
+    return visited
+def DFS(start,graph,visited=None):
+    if not visited:
+        visited=[]
+    visited.append(start)
+    for i in graph[start]:
+        if i not in visited:
+            DFS(i,graph,visited)
+    return visited
+
 
 graph={
     "A":["B","C"],
@@ -18,7 +40,20 @@ graph={
     "E":["C","D"]
 }
 
-
-res= BFS("E",graph)
+# graph={
+#     "A":["B","C"],
+#     "B":["A","C","D"],
+#     "C":["A","B","F"],
+#     "D":["B","E"],
+#     "E":["D"],
+#     "F":["C"]
+# }
+res1= BFS("D",graph)
+print()
+res= DFS("D",graph)
+print()
+print(res1)
+print()
 print(res)
 
+# ['E', 'C', 'D', 'A', 'B']
